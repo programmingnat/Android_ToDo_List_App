23495830902b06b5b409056cac5714c98c9c0f77
programmingnat <np7771@yahoo.com>
2016-06-14 08:23:22 -0400

    Added gridlayout to main list page

--- app/src/main/java/com/imaginat/androidtodolist/customlayouts/MainListFragment.java
+++ app/src/main/java/com/imaginat/androidtodolist/customlayouts/MainListFragment.java
@@ -3,14 +3,16 @@ package com.imaginat.androidtodolist.customlayouts;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.DimenRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -18,12 +20,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.imaginat.androidtodolist.MainActivity;
 import com.imaginat.androidtodolist.R;
-import com.imaginat.androidtodolist.businessModels.IListItem;
 import com.imaginat.androidtodolist.businessModels.ListManager;
 import com.imaginat.androidtodolist.businessModels.ListTitle;
 import com.imaginat.androidtodolist.businessModels.TheDeleter;
@@ -40,7 +40,7 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
     private static String TAG= MainListFragment.class.getName();
     private boolean isLongClickOn=false;
 
-    private TextView anchorTextView;
+    //private TextView anchorTextView;
 
     ReminderListRecycleAdapter mAdapter;
     ArrayList<ListTitle> mReminders;
@@ -63,15 +63,20 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-       View  view= inflater.inflate(R.layout.main_list_fragment, container, false);
+
+        View  view= inflater.inflate(R.layout.main_list_fragment, container, false);
         RelativeLayout rl = (RelativeLayout)view.findViewById(R.id.topPlaceHolder);
-        anchorTextView = (TextView)rl.findViewById(R.id.listItemTextView);
+        //anchorTextView = (TextView)rl.findViewById(R.id.listItemTextView);
         //mAddListOverlayView = (TextView) mTheAddingLayout.findViewById(R.id.addListEditText);
         mAdapter = new ReminderListRecycleAdapter((Context)getActivity(),mReminders=new ArrayList<ListTitle>(),this);
         RecyclerView recyclerView = (RecyclerView)view.findViewById(R.id.theRecyclerView);
         recyclerView.setAdapter(mAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        int orientation=GridLayoutManager.VERTICAL;
+        boolean reverseLayout=false;
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2,orientation,reverseLayout));
+        ItemOffsetDecoration itemDecoration = new ItemOffsetDecoration(getContext(),R.dimen.item_offset );
+        recyclerView.addItemDecoration(itemDecoration);
 
         //save color for later
         MainActivity mainActivity = (MainActivity)getActivity();
@@ -88,7 +93,7 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
 
         if (mIChangeToolbar != null) {
 
-                mIChangeToolbar.onUpdateTitle("MAIN");
+                mIChangeToolbar.onUpdateTitle("RemindME");
 
         }
 
@@ -98,108 +103,108 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
 
         //mAddListOverlayView.setHeight(itemFromList.getHeight());
 
-        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
-
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-
-                Log.d(TAG,"VALUE OF dy"+dy);
-                if(dy>0) {
-                    //test, just erase anything at first position
-                    LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                    View viewToHide = layoutManager.getChildAt(0);
-                    int firstVisible = layoutManager.findFirstVisibleItemPosition();
-                    if (viewToHide != null) {
-                        IListItem listItem = MainListFragment.this.mAdapter.getItem(firstVisible);
-                        anchorTextView.setText(listItem.getText());
-                        anchorTextView.setTextColor(Color.rgb(0, 0, 0));
-
-                        TextView tv = (TextView) viewToHide.findViewById(R.id.listItemTextView);
-                        String checkingText = tv.getText().toString();
-                        Log.d(TAG, "checking the reference to viewToHide, the text it has is " + checkingText);
-                        viewToHide.setBackgroundColor(Color.argb(0, 255, 255, 255));
-                        ((TextView) viewToHide.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(0, 0, 0, 0));
-                        //viewToHide.findViewById(R.id.competedRadioButton).setVisibility(View.INVISIBLE);
-                    }
-                }else if(dy<0){
-                    LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-
-
-                    int firstVisible = layoutManager.findFirstVisibleItemPosition();
-                    View viewToHide = layoutManager.getChildAt(0);
-                    View viewToShow = layoutManager.getChildAt(1);
-                    if (viewToShow != null) {
-                        IListItem listItem = MainListFragment.this.mAdapter.getItem(firstVisible>=0?firstVisible:0);
-                        Log.d(TAG,"the anchor will show "+listItem.getText());
-                        anchorTextView.setText(listItem.getText());
-                        anchorTextView.setTextColor(Color.rgb(0, 0, 0));
-
-                        TextView tv = (TextView) viewToShow.findViewById(R.id.listItemTextView);
-                        String checkingText = tv.getText().toString();
-                        Log.d(TAG, "reference to viewToShow, the text it has is " + checkingText);
-                        //viewToShow.setBackgroundColor(Color.argb(255, 255, 255, 255));
-                        viewToShow.setBackground(ResourcesCompat.getDrawable(getContext().getResources(), R.drawable.textlines, null));
-                        ((TextView) viewToShow.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(255, 0, 0, 0));
-                        //viewToShow.findViewById(R.id.competedRadioButton).setVisibility(View.VISIBLE);
-
-
-                        viewToHide.setBackgroundColor(Color.argb(0, 255, 255, 255));
-                        ((TextView) viewToHide.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(0, 0, 0, 0));
-                       // viewToHide.findViewById(R.id.competedRadioButton).setVisibility(View.INVISIBLE);
-                    }
-                }
-            }
-
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-
-                super.onScrollStateChanged(recyclerView, newState);
-
-
-
-
-                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-
-                int count = mAdapter.getItemCount();
-                if(count<=1){
-                    return;
-                }
-                Log.d(TAG, "inside onScrollStateChanged, first" + layoutManager.findFirstVisibleItemPosition());
-                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    View itemView = recyclerView.getChildAt(0);
-//                    if(itemView==null){
+//        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+//
+//            @Override
+//            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+//                super.onScrolled(recyclerView, dx, dy);
+//
+//                Log.d(TAG,"VALUE OF dy"+dy);
+//                if(dy>0) {
+//                    //test, just erase anything at first position
+//                    LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+//                    View viewToHide = layoutManager.getChildAt(0);
+//                    int firstVisible = layoutManager.findFirstVisibleItemPosition();
+//                    if (viewToHide != null) {
+//                        IListItem listItem = MainListFragment.this.mAdapter.getItem(firstVisible);
+//                        anchorTextView.setText(listItem.getText());
+//                        anchorTextView.setTextColor(Color.rgb(0, 0, 0));
+//
+//                        TextView tv = (TextView) viewToHide.findViewById(R.id.listItemTextView);
+//                        String checkingText = tv.getText().toString();
+//                        Log.d(TAG, "checking the reference to viewToHide, the text it has is " + checkingText);
+//                        viewToHide.setBackgroundColor(Color.argb(0, 255, 255, 255));
+//                        ((TextView) viewToHide.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(0, 0, 0, 0));
+//                        //viewToHide.findViewById(R.id.competedRadioButton).setVisibility(View.INVISIBLE);
+//                    }
+//                }else if(dy<0){
+//                    LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+//
+//
+//                    int firstVisible = layoutManager.findFirstVisibleItemPosition();
+//                    View viewToHide = layoutManager.getChildAt(0);
+//                    View viewToShow = layoutManager.getChildAt(1);
+//                    if (viewToShow != null) {
+//                        IListItem listItem = MainListFragment.this.mAdapter.getItem(firstVisible>=0?firstVisible:0);
+//                        Log.d(TAG,"the anchor will show "+listItem.getText());
+//                        anchorTextView.setText(listItem.getText());
+//                        anchorTextView.setTextColor(Color.rgb(0, 0, 0));
+//
+//                        TextView tv = (TextView) viewToShow.findViewById(R.id.listItemTextView);
+//                        String checkingText = tv.getText().toString();
+//                        Log.d(TAG, "reference to viewToShow, the text it has is " + checkingText);
+//                        //viewToShow.setBackgroundColor(Color.argb(255, 255, 255, 255));
+//                        viewToShow.setBackground(ResourcesCompat.getDrawable(getContext().getResources(), R.drawable.textlines, null));
+//                        ((TextView) viewToShow.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(255, 0, 0, 0));
+//                        //viewToShow.findViewById(R.id.competedRadioButton).setVisibility(View.VISIBLE);
+//
+//
+//                        viewToHide.setBackgroundColor(Color.argb(0, 255, 255, 255));
+//                        ((TextView) viewToHide.findViewById(R.id.listItemTextView)).setTextColor(Color.argb(0, 0, 0, 0));
+//                       // viewToHide.findViewById(R.id.competedRadioButton).setVisibility(View.INVISIBLE);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+//
+//                super.onScrollStateChanged(recyclerView, newState);
+//
+//
+//
+//
+//                LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+//
+//                int count = mAdapter.getItemCount();
+//                if(count<=1){
+//                    return;
+//                }
+//                Log.d(TAG, "inside onScrollStateChanged, first" + layoutManager.findFirstVisibleItemPosition());
+//                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+//                    View itemView = recyclerView.getChildAt(0);
+////                    if(itemView==null){
+////                        return;
+////                    }
+//                    int top = Math.abs(itemView.getTop());
+//                    int bottom = Math.abs(itemView.getBottom());
+//                    int scrollBy = top >= bottom ? bottom : -top;
+//                    if (scrollBy == 0) {
 //                        return;
 //                    }
-                    int top = Math.abs(itemView.getTop());
-                    int bottom = Math.abs(itemView.getBottom());
-                    int scrollBy = top >= bottom ? bottom : -top;
-                    if (scrollBy == 0) {
-                        return;
-                    }
-                    smoothScrollDeferred(scrollBy, (RecyclerView) recyclerView);
-                }
-                /*else {
-
-                    int currentIndex = layoutManager.findFirstVisibleItemPosition();
-
-                    if (currentIndex > lastFirstIndex) {
-                        Log.d(TAG, "SCROLLING UP (firstVisible:" + currentIndex + " vs lastFirstINdex:" + lastFirstIndex);
-                        scrollingUp(currentIndex,layoutManager);
-                    } else if (currentIndex < lastFirstIndex) {
-                        Log.d(TAG, "SCROLLING DOWN (firstVisible:" + currentIndex + " vs lastFirstINdex:" + lastFirstIndex);
-                        scrollingDown(currentIndex,layoutManager);
-                    } else {
-                        Log.d(TAG, "NO SCROLLING");
-                    }
-                    lastFirstIndex = currentIndex;
-
-
-                }
-
-*/
-            }
-        });
+//                    smoothScrollDeferred(scrollBy, (RecyclerView) recyclerView);
+//                }
+//                /*else {
+//
+//                    int currentIndex = layoutManager.findFirstVisibleItemPosition();
+//
+//                    if (currentIndex > lastFirstIndex) {
+//                        Log.d(TAG, "SCROLLING UP (firstVisible:" + currentIndex + " vs lastFirstINdex:" + lastFirstIndex);
+//                        scrollingUp(currentIndex,layoutManager);
+//                    } else if (currentIndex < lastFirstIndex) {
+//                        Log.d(TAG, "SCROLLING DOWN (firstVisible:" + currentIndex + " vs lastFirstINdex:" + lastFirstIndex);
+//                        scrollingDown(currentIndex,layoutManager);
+//                    } else {
+//                        Log.d(TAG, "NO SCROLLING");
+//                    }
+//                    lastFirstIndex = currentIndex;
+//
+//
+//                }
+//
+//*/
+//            }
+//        });
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -233,7 +238,7 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
     @Override
     public void onStart() {
         super.onStart();
-        mIChangeToolbar.onUpdateTitle("MAIN");
+        mIChangeToolbar.onUpdateTitle("RemindME");
 
     }
 
@@ -245,7 +250,7 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
         ArrayList<ListTitle>titles =listManager.getListTitles();
         mAdapter.setToRemindersArray(titles);
         mAdapter.notifyDataSetChanged();
-        mIChangeToolbar.onUpdateTitle("MAIN");
+        mIChangeToolbar.onUpdateTitle("RemindME");
 
 
     }
@@ -257,7 +262,7 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
         super.onAttach(context);
         try {
             mIChangeToolbar = (IChangeToolbar) context;
-            mIChangeToolbar.onUpdateTitle("MAIN");
+            mIChangeToolbar.onUpdateTitle("RemindME");
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -369,5 +374,27 @@ public class MainListFragment extends Fragment implements ReminderListRecycleAda
         ArrayList<ListTitle>titles =listManager.getListTitles();
         mAdapter.setToRemindersArray(titles);
         mAdapter.notifyDataSetChanged();
+    }
+
+
+
+    public class ItemOffsetDecoration extends RecyclerView.ItemDecoration {
+
+        private int mItemOffset;
+
+        public ItemOffsetDecoration(int itemOffset) {
+            mItemOffset = itemOffset;
+        }
+
+        public ItemOffsetDecoration(@NonNull Context context, @DimenRes int itemOffsetId) {
+            this(context.getResources().getDimensionPixelSize(itemOffsetId));
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+                                   RecyclerView.State state) {
+            super.getItemOffsets(outRect, view, parent, state);
+            outRect.set(mItemOffset, mItemOffset, mItemOffset, mItemOffset);
+        }
     }
 }
